a8f5ec4ba2e5b43ceaa0250cd606bac730bf4d5e
“Renato <serra.renato@gmail.com>
2016-11-30 00:22:55 -0200

    Primeiro WebService

--- src/br/com/estoque/entity/Livro.java
+++ src/br/com/estoque/entity/Livro.java
@@ -1,5 +1,64 @@
 package br.com.estoque.entity;
 
+import java.util.List;
+
 public class Livro {
+	private String nome;
+	private List<String> autores;
+	private String editora;
+	private Integer anoDePublicacao;
+	private String resumo;
+
+	public Livro() {
+
+	}
+
+	public Livro(String nome, List<String> autores, String editora, Integer anoDePublicacao, String resumo) {
+		this.nome = nome;
+		this.autores = autores;
+		this.editora = editora;
+		this.anoDePublicacao = anoDePublicacao;
+		this.resumo = resumo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<String> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<String> autores) {
+		this.autores = autores;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public Integer getAnoDePublicacao() {
+		return anoDePublicacao;
+	}
+
+	public void setAnoDePublicacao(Integer anoDePublicacao) {
+		this.anoDePublicacao = anoDePublicacao;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
 
 }
--- src/br/com/estoque/service/LivrosService.java
+++ src/br/com/estoque/service/LivrosService.java
@@ -1,5 +1,21 @@
 package br.com.estoque.service;
 
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import br.com.estoque.entity.Livro;
+import br.com.estoque.repository.LivroRepository;
+
+@WebService(endpointInterface = "br.com.estoque.service.LivrosService")
 public class LivrosService {
+	
+	@WebMethod 
+	public List<Livro> listarLivros() {
+		LivroRepository Livrorepositoy = new LivroRepository();
+		return Livrorepositoy.listarLivros();
+		}
+
 
 }
